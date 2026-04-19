d387e61530f67168b81e105fcd01346f47963ddb
Sasha Krigel <sasha.krigel@gmail.com>
2026-04-19 13:43:26 -0400

    attempting to make mount work

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from routes.generate import router as generate_router
+
 
 app = FastAPI(title="AltText Generator")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tighten this before prod
+    allow_origins=["*"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# app.include_router(alttext.router, prefix="/api")
+app.include_router(generate_router, prefix="/api")
+app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
 
-@app.get("/")
+@app.get("/api/hello")
 def greet_json():
     return {"Hello": "World!"}
 
